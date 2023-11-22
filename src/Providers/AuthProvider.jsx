97186ac04c83dc0d2bdaa5c9f5b20c974e333d98
tanvirsiraj <tanvirbinsiraj.edu.bd@gmail.com>
2023-11-22 13:41:43 +0600

    provider setup

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,74 @@
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const googleSignIn = () => {
+    return signInWithPopup(auth, provider);
+  };
+
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
+      // console.log('user inside the auth state observer', loggedUser);
+      setUser(loggedUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    createUser,
+    signIn,
+    logOut,
+    googleSignIn,
+    updateUserProfile,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
